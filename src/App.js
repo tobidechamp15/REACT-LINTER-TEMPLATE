f13cc75e0bce92068894ed43d42bb62dc6ccaf51
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-06-14 19:13:19 +0100

    main changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,9 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
           <code>src/App.js</code>
-          {' '}
           and save to reload.
         </p>
         <a
